095ce1ac8d45230fb69b396f44af8771c10cf40a
alka.muneesamy <alkak22@yahoo.com>
2021-01-14 15:57:12 +0000

    add fixes to actions index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -31,13 +31,11 @@ import {FETCH_USER,
         DELETE_SUBCAT_QUESTION
       } from "./types";
 
-export const fetchDistance = () =>
-{
-  return ( async (dispatch) => {
-    const response = await addressDistance.get("/json?origins");
-    dispatch({type: FETCH_DISTANCE, payload: response});
-  });
-}
+export const fetchDistance = () =>  async dispatch => {
+    const res = await addressDistance.get("/json?origins");
+    dispatch({type: FETCH_DISTANCE, payload: res});
+  };
+
 
 export const fetchUser = () =>
 {
@@ -47,188 +45,153 @@ export const fetchUser = () =>
  })
 }
 //action creators for Categories master
-export const addCategory = (values) => {
-   return( async (dispatch) => {
+export const addCategory = (values) =>  async dispatch => {
     const res = await axios.post("http://localhost:5000/api/category",values);
     dispatch({type: CREATE_CATEGORY, payload: res.data });
     history.push("/admin/categories/categorieslist");
-  });
-};
-
-export const fetchCategories = () => async (dispatch) => {
-   const res= await axios.get("http://localhost:5000/api/category");
-   console.log("Response from fetch categories:",res.data);
-   dispatch({type: FETCH_CATEGORIES, payload: res.data});
-     
   };
 
-  export const editCategory = (id, formValues) => {
-    return( async (dispatch) => {
+export const fetchCategories = () => async dispatch => {
+   const res= await axios.get("http://localhost:5000/api/category");
+   dispatch({type: FETCH_CATEGORIES, payload: res.data});     
+  };
+
+  export const editCategory = (id, formValues) => async dispatch => {
       const res= await axios.patch(`http://localhost:5000/api/category/${id}`,formValues);
       dispatch({type:EDIT_CATEGORY, payload: res.data});
-      history.push("/admin/categories/categorieslist");
-    });
-  };
+      history.push("/admin/categories/categorieslist"); 
+   };  
 
-export const fetchCategory = (id) => {
-  return( async (dispatch) => {
+ export const fetchCategory = (id) => async dispatch => {
     const res= await axios.get(`http://localhost:5000/api/category/${id}`);
     dispatch({type: FETCH_CATEGORY, payload:res.data});
-  });
-};
+  };
 
-export const deleteCategory =(id) => {
-  return( async (dispatch) => {
+export const deleteCategory =(id) => async dispatch => {
   await axios.delete(`http://localhost:5000/api/category/${id}`); 
   dispatch({type: DELETE_CATEGORY, payload: id});
   history.push("/admin/categories/categorieslist");
-  });
-};
+  };
 
  //action creator for Subcategory Master
- export const addSubcategory = (values) => async (dispatch) => {
+ export const addSubcategory = (values) => async dispatch => {
    const res = await axios.post("http://localhost:5000/api/subcategory",values);
    dispatch({type: CREATE_SUBCATEGORY, payload: res.data});
    history.push("/admin/subcategories/subcategorieslist");
- }
+ };
 
- export const fetchSubcategories = () => async (dispatch) => {
+ export const fetchSubcategories = () => async dispatch => {
     const res = await axios.get("http://localhost:5000/api/subcategory");
     dispatch({ type: FETCH_SUBCATEGORIES, payload:res.data });
- }
+ };
 
- export const fetchSubcategory = (id) => async (dispatch) => {
+ export const fetchSubcategory = (id) => async dispatch => {
     const res = await axios.get(`http://localhost:5000/api/subcategory/${id}`);
     dispatch({ type: FETCH_SUBCATEGORY, payload: res.data});
- }
+ };
 
- export const editSubcategory = (id, formValues) => async (dispatch) => {
+ export const editSubcategory = (id, formValues) => async dispatch => {
    console.log("Formvalues from edit subcategory",formValues);
     const res = await axios.patch(`http://localhost:5000/api/subcategory/${id}`,formValues);
     dispatch( { type: EDIT_SUBCATEGORY, payload: res.data});
     history.push("/admin/subcategories/subcategorieslist");
- }
+ };
 
-export const deleteSubcategory = (id) => {
-    return( async (dispatch) => {
+export const deleteSubcategory = (id) => async dispatch => {
     await axios.delete(`http://localhost:5000/api/subcategory/${id}`);
     dispatch({type: DELETE_SUBCATEGORY, payload: id });
     history.push("/admin/subcategories/subcategorieslist");
-  })
-}
+  };
+
 
  //action creator for Questions Master
- export const addQuestion = (values) => {
-   return( async (dispatch) => {
+ export const addQuestion = (values) => async dispatch => {
       console.log("In questions action creator", values);
        const res = await axios.post("http://localhost:5000/api/questions", values);
        dispatch({type: CREATE_QUESTION, payload: res.data});
        history.push("/admin/questions/questionslist");
-   })
- }
+   }; 
 
- export const fetchQuestions = () => {
-   return (async (dispatch) => {
+ export const fetchQuestions = () => async dispatch => {
      const res = await axios.get("http://localhost:5000/api/questions");
      dispatch({ type: FETCH_QUESTIONS, payload: res.data})
-   })
- }
+   }; 
 
- export const fetchQuestion = (id) => {
-   return( async (disptach) => {
+ export const fetchQuestion = (id) => async dispatch => {
      const res= await axios.get(`http://localhost:5000/api/questions/${id}`) ;
-     disptach({ type: FETCH_QUESTION, payload: res.data})
-   })
- }
+     dispatch({ type: FETCH_QUESTION, payload: res.data})
+   }; 
 
- export const editQuestion = (id,formValues) => {
-   return( async (dispatch) => {
+ export const editQuestion = (id,formValues) => async dispatch => {
      const res= await axios.patch(`http://localhost:5000/api/questions/${id}`,formValues);
      dispatch({ type: EDIT_QUESTION, payload: res.data});
      history.push("/admin/questions/questionslist");
-   })
- }
+   }; 
 
- export const deleteQuestion = (id) => {
-   return( async (dispatch) => {
+ export const deleteQuestion = (id) => async dispatch => {
      const res= await axios.delete(`http://localhost:5000/api/questions/${id}`);
      console.log("Response from delete question:",res.data);
      dispatch({ type: DELETE_QUESTION, payload: res.data});
      history.push("/admin/questions/questionslist");
-   })
- }
+  }; 
 
  //action creator for subcategory questions
- export const addSubcatQuestion = (values) => {
-     return( async (dispatch) => {
+ export const addSubcatQuestion = (values) => async dispatch => {
      const res= await axios.post("http://localhost:5000/api/subcatquestion",values);
      dispatch({type: CREATE_SUBCAT_QUESTION, payload: res.data })
-   })
- }
+   };
 
- export const fetchSubcatQuestions = () => {
-   return( async (dispatch) => {
+ export const fetchSubcatQuestions = () => async dispatch => {
    const res= await axios.get("http://localhost:5000/api/subcatquestion");
    dispatch({ type: FETCH_SUBCAT_QUESTIONS, payload: res.data})
-   });
- }
+   }; 
 
- export const fetchSubcatQuestion = (id) => {
-   return( async (dispatch) => {
+ export const fetchSubcatQuestion = (id) => async dispatch => {
    const res= await axios.get(`http://localhost:5000/api/subcatquestion/${id}`);
    dispatch({ type: FETCH_SUBCAT_QUESTION, payload: res.data})
-   });
- }
+   }; 
 
- export const editSubcatQuestion = (id,formValues) => {
-   return( async (dispatch) => {
+ export const editSubcatQuestion = (id,formValues) => async dispatch => {
    const res = await axios.patch(`http://localhost:5000/api/subcatquestion/${id}`,formValues) ;
    dispatch({ type: EDIT_SUBCAT_QUESTION, payload: res.data}) 
-   })
- }
+  }; 
 
- export const deleteSubcatQuestion = (id) => {
-   return (async (dispatch) => {
+ export const deleteSubcatQuestion = (id) => async dispatch => {
    const res = await axios.delete(`http://localhost:5000/api/subcatquestion/${id}`);
    dispatch( { type: DELETE_SUBCAT_QUESTION, payload: res.data })  
-   });
- }
- 
+   };
+  
 
 //action creator for Vendors
-export const submitVendor = (values, history) => {
-  return ( async (dispatch) => {
+export const submitVendor = (values, history) => async dispatch => {
     console.log("values from vendor action creator:",values);
      const res = await axios.post("http://localhost:5000/api/vendor", values);
          //  history.push("/");
     //  window.location.href = '/'
      dispatch({type: SUBMIT_VENDOR, payload:res.data});
-  })    
-}
+  }; 
 
-export const submitVendorCategories = (values,history) => {
-   return (async (dispatch) => {
+export const submitVendorCategories = (values,history) => async dispatch => {
      const res= await axios.post("http://localhost:5000/api/vendorcategories",values);
      //  history.push("/");
     window.location.href = '/'
-   })
-}
+ };
 
 //util for fetching the list of category names
-export const fetchCategoriesName = () => async (dispatch) => {
+export const fetchCategoriesName = () => async dispatch => {
   const res= await axios.get("http://localhost:5000/api/categoryname");  
   dispatch({type: FETCH_CATEGORIES_NAME, payload: res.data});    
  };
 
 //util for fetching the list of subcategory names
- export const fetchSubcategoriesName = () => async (dispatch) => {  
+ export const fetchSubcategoriesName = () => async dispatch => {  
    const res= await axios.get("http://localhost:5000/api/subcategoryname");
    console.log("Response from fetch subcategories name:", res.data);
    dispatch( {type: FETCH_SUBCATEGORIES_NAME, payload: res.data});
  }
 
  //util for fetching the list of questions
- export const fetchQuestionsName = () => async (dispatch) => {
+ export const fetchQuestionsName = () => async dispatch => {
    const res= await axios.get("http://localhost:5000/api/questionname");
    dispatch( {type: FETCH_QUESTIONS_NAME, payload: res.data});
  }
