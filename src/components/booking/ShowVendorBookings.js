2ea598707aa432e9dc59d33656a9527dd1d54dda
Alka Muneesamy <alkak22@yahoo.com>
2021-05-16 10:32:17 +0100

    changes for vendor booking

---
+++ src/components/booking/ShowVendorBookings.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ShowVendorBookings = () => {
+
+    return (
+
+        <div>
+            
+        </div>
+
+
+    );
+
+}
+
+export default ShowVendorBookings;
